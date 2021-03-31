e85234a4425fe25c9cb93575f5f6d8b0439216b0
ishaanjav <ishaanjav@gmail.com>
2021-03-31 13:06:44 -0500

    More features

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # This is all the setup you have to do
 # just provide the Zoom links you want to join and the time they should be joined in the format
-# HH:MM:SS Ex: 08:05:59
+# HH:MM:SS Ex: 08:05:59    or  13:30:00 for 1:30 PM
 
 # NOTE: The links should be ordered by earliest join time first.
 
@@ -9,3 +9,6 @@ links = [
 ["https://us02web.zoom.us/......", "time here"],
 ["https://us02web.zoom.us/......", "time here"]
 ]
+
+# OPTIONAL: provide the names of the meetings
+names = ["name 1", "name 2"]
--- zoom.py
+++ zoom.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from time import sleep
 
 links = setup.links
+names = setup.names
 if (len(links) == 0):
 	print("No links provided")
 	quit()
@@ -14,7 +15,6 @@ idx = 0
 found = False
 orig = datetime.now()
 now = orig.strftime("%H:%M:%S")
-
 # find the zoom link that will have to be joined next
 for i in range(len(links)):
 	idx = i
@@ -30,8 +30,23 @@ startTime = datetime.strptime(links[idx][1], "%H:%M:%S")
 startTime = startTime.replace(year=orig.year, month=orig.month, day=orig.day)
 
 # Difference in seconds between time to join and current time
-waitTime = (int) ((startTime - orig).total_seconds()) - 5
-print("Waiting: ", waitTime, "seconds")
+waitTime = (int) ((startTime - orig).total_seconds()) - 4
+hours = int(waitTime / 3600)
+minutes = int((waitTime - 3600 * hours) / 60)
+seconds = waitTime % 60
+
+print("Joining ", end = '')
+if len(names) > idx and names[idx] != ("name " + str(idx + 1)):
+	print(names[idx], end = '')
+print(" in ", end='')
+if hours != 0:
+	print(hours, "hrs ", end = '')
+if minutes != 0:
+	print(minutes, "min ", end = '')
+if seconds != 0:
+	print(seconds, "sec ", end = '')
+
+print()
 
 sleep(waitTime)
 join(links[idx][0])
